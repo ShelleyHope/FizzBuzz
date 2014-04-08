580ba4319ea7e0047701fa9e4f49aa4a1459ec33
shelleyhope <shelleyandkenny@mac.com>
2014-04-08 14:34:16 +0100

    6m 34s

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -4,15 +4,12 @@ end
 
 def is_divisible_by_5(number)
 	number % 5 == 0
-end
+	end
 
 def fizzbuzz(number)
 	return "FizzBuzz" if is_divisible_by_3(number) && is_divisible_by_5(number)
-	return "Fizz" if is_divisible_by_3(number)
 	return "Buzz" if is_divisible_by_5(number)
+	return "Fizz" if is_divisible_by_3(number)
 	number
 end
-
-
-
 
