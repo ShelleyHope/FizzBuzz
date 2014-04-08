4a1dfe9ca5f4d57e5ee3739346788185177f6a1a
shelleyhope <shelleyandkenny@mac.com>
2014-04-08 09:15:00 +0100

    finished exercise for 2nd time

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,20 @@
+def is_divisible_by_3(number)
+	number % 3 == 0
+end
+
+def is_divisible_by_5(number)
+	number % 5 == 0
+end
+
+def is_divisible_by_15(number)
+	number % 15 == 0
+end
+
+def fizzbuzz(number)
+	return "FizzBuzz" if is_divisible_by_15(number)
+	return "Fizz" if is_divisible_by_3(number)
+	return "Buzz" if is_divisible_by_5(number)
+	number
+end
+
+
