a9e29220979194fbd8238a2da37effaab7d6935f
shelleyhope <shelleyandkenny@mac.com>
2014-04-08 14:58:08 +0100

    wiping the slate clean again last time was 7m 17s

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -4,12 +4,16 @@ end
 
 def is_divisible_by_5(number)
 	number % 5 == 0
-	end
+end
 
 def fizzbuzz(number)
 	return "FizzBuzz" if is_divisible_by_3(number) && is_divisible_by_5(number)
-	return "Buzz" if is_divisible_by_5(number)
 	return "Fizz" if is_divisible_by_3(number)
+	return "Buzz" if is_divisible_by_5(number)
 	number
 end
+
+
+
+
 
