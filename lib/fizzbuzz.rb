6e6d3836de4d57ad74aa2f9e2bd4c41c2bc9b54f
shelleyhope <shelleyandkenny@mac.com>
2014-04-08 11:58:16 +0100

    groundhog day

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -6,14 +6,11 @@ def is_divisible_by_5(number)
 	number % 5 == 0
 end
 
-def is_divisible_by_15(number)
-	number % 15 == 0
-end
-
-def fizzbuzz(number) 
+def fizzbuzz(number)
 	return "FizzBuzz" if is_divisible_by_3(number) && is_divisible_by_5(number)
 	return "Fizz" if is_divisible_by_3(number)
 	return "Buzz" if is_divisible_by_5(number)
 	number
 end
+
 
